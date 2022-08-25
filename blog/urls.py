fd1b237f6246924b3d10f1e82ea63293088a1af0
pankti77 <pankti77dave@gmail.com>
2022-08-25 10:40:25 +0530

    added image

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -6,4 +8,4 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('<slug:tag_slug>/<slug:slug>/', views.detail, name='post_detail'),
     path('<slug:slug>/', views.tag, name='tag_detail'),
-]
+] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
